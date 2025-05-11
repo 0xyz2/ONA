4aeeb2706f20f4299c4e91d53dc89ca7b283a754
Catalina <catalinagordillopena@gmail.com>
2025-05-10 23:04:35 -0500

    Definición del endpoint POST para crear usuarios

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -1,3 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const userSchema = require("../models/user");
+
+router.post("/usuarios", (req, res) => {
+  const user = new userSchema(req.body);
+  user
+    .save()
+    .then((data) => res.json(data))
+    .catch((error) => res.status(400).json({ message: error }));
+});
