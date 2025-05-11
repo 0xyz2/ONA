a423a32b2402674790bf45d2eedaff3cf7a5c25f
Catalina <catalinagordillopena@gmail.com>
2025-05-10 23:01:12 -0500

    creacion de campos de usuarios

---
+++ src/models/usuario.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true,
+  },
+  correo: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  contraseña: {
+    type: String,
+    required: true,
+  },
+  rol: {
+    type: String,
+    enum: ["usuario", "moderador", "admin"],
+    default: "usuario",
+  },
+});
