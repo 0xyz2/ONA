9d83c8f26af6a79384a8b9e0d7e45ac720556fe8
SaraMurcia <saramurciarojas13@gmail.com>
2025-05-13 19:12:21 -0500

    Creación Campos de "Publicacion"
    se crearon los campos respectivos de publicación

---
+++ src/models/publicacion.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const publicacionSchema = mongoose.Schema({
+  usuario: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fecha: {
+    type: Date,
+    required: true
+  },
+  descripcion: {
+    type: String,
+    required: true,
+  },
+  categoria: {
+    type: String,
+    enum: ["moda", "salud ", "estudio", "tecnología", "política"]
+  }
+});
+module.exports = mongoose.model("Publicacion", publicacionSchema);
