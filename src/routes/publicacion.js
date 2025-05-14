ce816d42dad4931aadb239cef0b84213324f2dc3
SaraMurcia <saramurciarojas13@gmail.com>
2025-05-13 23:29:54 -0500

    Agregación metodo GET en publicaiones.js
    Se agrego método GET para buscar todas las publicaciones y buscar las publicaciones por categoría

--- src/routes/publicacion.js
+++ src/routes/publicacion.js
@@ -10,6 +10,23 @@ router.post("/publicacion", (req, res) => {
     .catch((error) => res.status(400).json({ message: error }));
 });
 
+router.get("/publicaciones/categoria/:categoria", (req, res) => {
+    const { categoria } = req.query;
+
+    const filtro = categoria ? { categoria } : {};
+
+    publicacionSchema.find(filtro)
+        .then((data) => res.json(data))
+        .catch((error) => res.status(500).json({ message: error }));
+});
+
+router.get("/publicaciones", (req, res) => {
+    publicacionSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+
 router.delete("/publicacion/:id", (req, res) => {
     const { id } = req.params;
     publicacionSchema
