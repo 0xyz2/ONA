130afb2586de7f451c55dc63f0849233733fb73e
SantIInge <maicolsebancho@gmail.com>
2025-05-18 11:54:23 -0500

    Creacion de modulo reportes.js

---
+++ src/models/reportes.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const reporteSchema = mongoose.Schema({
+    
+    publicacionId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Publicacion",
+    required: true
+  },
+
+  motivo: {
+    type: String,
+    required: true
+  },
+
+  reportadoPor: {
+    type: String,
+    required: true
+  },
+
+   fecha: {
+    type: Date,
+    default: Date.now
+  },
+
+
+});
+
+module.exports = mongoose.model("Reporte", reporteSchema);
